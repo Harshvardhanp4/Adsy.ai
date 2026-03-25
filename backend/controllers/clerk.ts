51f775003981b68bd68b5a5c36ed101f0ed24d52
Harshvardhanp4 <harshupatil.224@gmail.com>
2026-03-25 08:55:22 +0530

    COMMIT: Webhook setup done

---
+++ backend/controllers/clerk.ts
@@ -0,0 +1,93 @@
+import { verifyWebhook } from "@clerk/express/webhooks"
+import { Request, Response } from "express"
+import { prisma } from "../config/prisma.js";
+
+const clerkWebhooks = async (req: Request, res: Response) => {
+    try {
+        const evt: any = await verifyWebhook(req)
+        // Get data from request
+
+        const { data, type } = evt;
+
+        // Switch case for diff events
+
+        switch (type) {
+            case "user.created": {
+                await prisma.user.create({
+                    data: {
+                        id: data.id,
+                        email: data?.email_addresses[0]?.email_address,
+                        name: data?.first_name + " " + data?.last_name,
+                        image: data?.image_url,
+
+                    }
+                })
+                break;
+            }
+
+            case "user.updated": {
+                await prisma.user.update({
+                    where: {
+                        id: data.id,
+                    },
+                    data: {
+                        email: data?.email_addresses[0]?.email_address,
+                        name: data?.first_name + " " + data?.last_name,
+                        image: data?.image_url,
+
+                    }
+                })
+                break;
+            }
+
+            case "user.deleted": {
+                await prisma.user.deleteMany({
+                    where: {
+                        id: data.id
+                    }
+                })
+                break;
+            }
+
+            case "paymentAttempt.updated": {
+                if ((data.charge_type === "recurring" || data.charge_type === "checkout") && data.status === "paid") {
+                    const credits = { pro: 80, premium: 240, }
+                    const clerkUserId = data?.payer?.user_id;
+                    const planId: keyof typeof credits = data?.subscription_items?.[0]?.plan?.slug;
+
+                    if (planId !== "pro" && planId !== "premium") {
+                        return res.status(400).json({ msg: "Invalid plan" })
+                    }
+
+                    console.log(planId)
+
+                    await prisma.user.update({
+                        where: {
+                            id: clerkUserId
+                        },
+                        data: {
+                            credits: { increment: credits[planId] }
+                        }
+                    })
+                }
+                break;
+            }
+
+
+            default:
+                break;
+        }
+
+        res.json({
+            msg: "Webhook received: " + type
+        })
+
+    } catch (error: any) {
+        console.error("Clerk webhook error:", error)
+        res.status(500).json({
+            msg: error?.message || "Clerk WH Error"
+        })
+    }
+}
+
+export default clerkWebhooks;
